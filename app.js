b1c91950d039ec88cfc070f9fdbbc1be796b3480
Milito11 <mariomilito14@gmail.com>
2021-08-14 22:49:36 -0300

    Estructura de archivos Node.js+Express

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require ("express");
+const app = express();
+
+const path = require ("path");
+
+app.listen (4040, 
+    console.log("Servidor funcionando")
+);
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/home.html"));
+});
