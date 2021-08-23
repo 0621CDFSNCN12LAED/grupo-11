b29ca24cb9a864aaa1aacf826392e9ce340f7679
EliasLazo <elias.lazo.2021@gmail.com>
2021-08-23 20:22:34 -0300

    Pagina detalle del servicio

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
 
-const path = require ("path");
+const path = require("path");
 
-app.listen (4040, 
-    console.log("Servidor funcionando")
-);
+app.listen(4040, console.log("Servidor funcionando"));
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "views/home.html"));
+  res.sendFile(path.join(__dirname, "views/home.html"));
 });
 
+app.get("/detailService", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/detailService.html"));
+});
