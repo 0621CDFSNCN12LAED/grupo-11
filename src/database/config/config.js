5bcf482e556bae5db5c164e748759eaaccfae47b
Milito11 <mariomilito@gmail.com>
2021-11-25 18:42:37 -0300

    Cambié la configuración de la base de datos en config.js

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   development: {
     username: "root",
-    password: null,
-    database: "data",
+    password: "grupo11",
+    database: "ola_db",
     host: "127.0.0.1",
     dialect: "mysql",
   },
