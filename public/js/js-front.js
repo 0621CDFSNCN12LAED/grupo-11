34622f5d7aaf39d5f103f58565f44ca5885dfb63
Milito11 <mariomilito@gmail.com>
2021-12-13 04:39:24 -0300

    Validación de mail con validator

--- public/js/js-front.js
+++ public/js/js-front.js
@@ -1,35 +1,35 @@
-window.addEventListener("load", function(){
-    let formulario = document.querySelector("form.register");
-    formulario.addEventListener("submit", function(e){
-        
-        let errores = [];
-        
-        let campoNombre =  document.querySelector("input.name");
-        if(campoNombre.value == ""){
-            errores.push("Este campo es obligatorio");
-        } else if (campoNombre.value.length < 2) {
-            errores.push("Debe tener al menos 2 caracteres")
-        }
-        
-        let campoEmail = document.querySelector("input.email");
-        if (campoEmail.value == "") {
-          errores.push("Este campo es obligatorio");
-        } else if () {
-          errores.push("Debe ser un mail válido");
-        }
-        
-        let campoContraseña = document.querySelector("input.password");
-        if (campoContraseña.value == "") {
-          errores.push("Este campo es obligatorio");
-        } else if (campoNombre.value.length < 8) {
-          errores.push("Debe tener al menos 8 caracteres");
-        }
-        if(errores.length > 0){
-            e.preventDefault();
-            let ulErrores = document.querySelector("div.errores ul");
-            for (let i = 0; i < errores.length; i++){
-                ulErrores.innerHTML += "<li>" + errores[i] + "</li>"
-            }
-        }
-    })
-})
+const validator = require("validator");
+window.addEventListener("load", function () {
+  let formulario = document.querySelector("form.register");
+  formulario.addEventListener("submit", function (e) {
+    let errores = [];
+
+    let campoNombre = document.querySelector("input.name");
+    if (campoNombre.value == "") {
+      errores.push("Este campo es obligatorio");
+    } else if (campoNombre.value.length < 2) {
+      errores.push("Debe tener al menos 2 caracteres");
+    }
+
+    let campoEmail = document.querySelector("input.email");
+    if (campoEmail.value == "") {
+      errores.push("Este campo es obligatorio");
+    } else if (validator.isEmail(campoEmail.value)) {
+      errores.push("Debe ser un mail válido");
+    }
+
+    let campoContraseña = document.querySelector("input.password");
+    if (campoContraseña.value == "") {
+      errores.push("Este campo es obligatorio");
+    } else if (campoNombre.value.length < 8) {
+      errores.push("Debe tener al menos 8 caracteres");
+    }
+    if (errores.length > 0) {
+      e.preventDefault();
+      let ulErrores = document.querySelector("div.errores ul");
+      for (let i = 0; i < errores.length; i++) {
+        ulErrores.innerHTML += "<li>" + errores[i] + "</li>";
+      }
+    }
+  });
+});
