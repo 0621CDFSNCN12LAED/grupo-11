ff7e23f18d952cfd57057ccb4e1460329cd98e3f
Milito11 <mariomilito@gmail.com>
2022-01-03 11:31:37 -0300

    Reacomodando carpetas

--- Dashboard/src/components/contentWrapper/contentRowTop/contentRowTop.js
+++ Dashboard/src/components/contentWrapper/contentRowTop/contentRowTop.js
@@ -16,12 +16,7 @@ function ContentRowTop() {
         color="primary"
         value="16.599"
       />
-      <ValueCard
-        title="Profesiones"
-        icon="fa-film"
-        color="primary"
-        value="21"
-      />
+      <ValueCard title="Profesiones" icon="fa-film" color="primary" value="8" />
     </div>
   );
 }
